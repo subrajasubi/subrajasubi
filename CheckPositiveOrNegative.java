45ef343049cc97ef60f9587f0ae202edf8fc0939
subrajasubi <95363799+subrajasubi@users.noreply.github.com>
2021-12-02 11:11:18 +0530

    Add files via upload

---
+++ CheckPositiveOrNegative.java
@@ -0,0 +1,33 @@
+package week1.day2.assignments;
+
+
+public class CheckPositiveOrNegative {
+
+	public static void main(String[] args) {
+		
+		
+		int number = 35;	
+
+	
+	if (number >0) {
+		
+		System.out.println("The Number is Positive:" +number);
+	}
+	else if(number < 0) {
+		
+		System.out.println("The Number is Negative" +number);
+	}
+	else
+	{
+		System.out.println(" The number is Positive nor Negative" +number);
+
+	}
+	
+	}
+}
+	
+	
+	
+	
+	
+	
