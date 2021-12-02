08a92a0fea513f63270a44577e0984f943c1304a
subrajasubi <95363799+subrajasubi@users.noreply.github.com>
2021-12-02 16:35:52 +0530

    Add files via upload

---
+++ ArraysMissingElements.java
@@ -0,0 +1,36 @@
+package week1.day2.assignments;
+
+import java.util.Arrays;
+
+
+public class ArraysMissingElements {
+
+	public static void main(String[] args) {
+		
+		int[] nums = {1,2,3,4,7,6,8};	
+		Arrays.sort(nums);
+		
+		for (int i = 1; i <nums.length; i++) {
+			
+			
+			if(i!= nums[i-1])	
+				
+				
+			
+	System.out.println(i);
+		
+		}
+		
+		}
+			
+		
+		
+	}
+
+	
+			
+		
+	
+
+
+
