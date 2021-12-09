0f030558d54c7f6a4435a0464700e20ab2f031f8
subrajasubi <95363799+subrajasubi@users.noreply.github.com>
2021-12-09 15:56:12 +0530

    Add files via upload

---
+++ Assignment4DropXpath.java
@@ -0,0 +1,76 @@
+package week2.day2.assignments;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignment4DropXpath {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+
+		ChromeDriver driver = new ChromeDriver();
+
+		driver.get("http://leafground.com/pages/Dropdown.html");
+
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		String title = driver.getTitle();
+
+		System.out.println("Title:" + title);
+
+		WebElement DD1 = driver.findElement(By.id("dropdown1"));
+
+		Select sec = new Select(DD1);
+
+		sec.selectByIndex(1);
+
+		WebElement DD2 = driver.findElement(By.name("dropdown2"));
+
+		Select sec1 = new Select(DD2);
+
+		sec1.selectByVisibleText("Selenium");
+
+		WebElement DD3 = driver.findElement(By.id("dropdown3"));
+
+		Select sec2 = new Select(DD3);
+
+		sec2.selectByValue("1");
+
+		WebElement findElement = driver.findElement(By.xpath("//select[@class='dropdown']"));
+
+//findElement.getSize();
+
+		
+		  Select options = new Select(findElement);
+		  
+		  List<WebElement> options2 = options.getOptions();
+		  
+		  int size = options2.size();
+		  
+		  System.out.println(size);
+		  
+//		 System.out.println("The number is:"
+			// +driver.findElement(By.className("dropdown")).getAttribute("value"));
+
+		// driver.findElement(By.className("example")).sendKeys("Selenium");
+
+		  Thread.sleep(5000);
+	//	  driver.findElement(By.xpath("(//div[@class='example'])[5]")).click();
+		  
+		  driver.findElement(By.xpath("(//div[@class='example'])[5]/select")).sendKeys("Selenium");
+		  
+		driver.findElement(By.xpath("//option[text()='Select your programs']/following-sibling::option")).click();
+
+	}
+
+}
