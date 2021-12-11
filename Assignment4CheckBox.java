905220aa62872817d0916181efe8b69367353884
subrajasubi <95363799+subrajasubi@users.noreply.github.com>
2021-12-11 13:18:00 +0530

    Add files via upload

--- Assignment4CheckBox.java
+++ Assignment4CheckBox.java
@@ -31,42 +31,44 @@ public class Assignment4CheckBox {
 			
 			driver.findElement(By.xpath("//div[@class='example']//input[1]")).click();
 			
-			WebElement selected = driver.findElement(By.xpath("//div[text()='Selenium']"));
-			
-			 boolean enabled = selected.isEnabled();
-	           
-	           System.out.println(enabled);
-	           
-	           
-	        
-	          
-	          WebElement deselect = driver.findElement(By.xpath("//label[text()='DeSelect only checked']/following::input"));
-	          
-	          if(deselect.isSelected()) {
-	        	  
-	        	  deselect.click();
-	        	  
-	          }
-	          
-	          WebElement de = driver.findElement(By.xpath("//label[text()='DeSelect only checked']/following::input[2]"));
-	          
-	          if(de.isSelected()) {
-	        	  
-	        	  de.click();
-	          }
-	          
-	          Thread.sleep(2000);	 
-	          
-	          driver.findElement(By.xpath("//div[text()='Option 1']")).click();
-	          
-	          driver.findElement(By.xpath("//div[text()='Option 2']")).click();
-	          
-	          driver.findElement(By.xpath("//div[text()='Option 3']")).click();
-	          
-	          driver.findElement(By.xpath("//div[text()='Option 4']")).click();
-	          
 		
+			
+			System.out.println(driver.findElement(By.xpath("//div[text()='Selenium']")).isEnabled());
+			
+		
+	           
+	     WebElement deselect = driver.findElement(By.xpath("//label[text()='DeSelect only checked']/following::input"));      
+	        
+	     if (deselect.isSelected()) {
+			
+	     deselect.click();
+	    	 
+		} 
+	     WebElement de = driver.findElement(By.xpath("//label[text()='DeSelect only checked']/following::input[2]"));
+	         
+	     if (de.isSelected()) {
+			
+	    	 de.click();
+	    	 
+		}
+
+	     
+	     driver.findElement(By.xpath("//label[text()='Select all below checkboxes ']/following::input[1]")).click();
+	     
+	     driver.findElement(By.xpath("//label[text()='Select all below checkboxes ']/following::input[2]")).click();
+	     
+	     driver.findElement(By.xpath("//label[text()='Select all below checkboxes ']/following::input[3]")).click();
+	     
+	     driver.findElement(By.xpath("//label[text()='Select all below checkboxes ']/following::input[4]")).click();
+	        
+	     driver.findElement(By.xpath("//label[text()='Select all below checkboxes ']/following::input[5]")).click();
+	     	
+	        	 
+			}
+	          
+	       
 		
 	}
+	
 
-}
+
