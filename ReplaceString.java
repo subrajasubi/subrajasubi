a0184b71ad9b8cb6738a043744c0088c95275919
subrajasubi <95363799+subrajasubi@users.noreply.github.com>
2021-12-15 15:17:55 +0530

    Add files via upload

---
+++ ReplaceString.java
@@ -0,0 +1,30 @@
+package week3.day1.assignments;
+
+public class ReplaceString {
+
+	public static void main(String[] args) {
+		String sen = "I am working with Java8";
+	     
+	     sen = 	sen.replace("8","11" );
+	     
+	    System.out.println("Replaced 8to 11:" +sen);
+		
+	    sen = sen.concat("#");
+	    
+	    System.out.println("Replacing with #:" +sen);
+	    
+	    sen = sen.substring(5,11);
+	    
+	    System.out.println("Substring:" +sen);
+		}
+		
+		
+		}
+			
+		
+		
+		
+
+	
+
+
