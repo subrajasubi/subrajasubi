0803ba11086ad32fefe07fffa91f4f8ce166034d
subrajasubi <95363799+subrajasubi@users.noreply.github.com>
2021-12-15 15:06:09 +0530

    Add files via upload

---
+++ CompareString.java
@@ -0,0 +1,39 @@
+package week3.day1.assignments;
+
+public class CompareString {
+
+	public static void main(String[] args) {
+		
+		        String str1="I am Learning Java" ;
+				String str2="I am learning java" ;
+				
+				//for (int i = 0; i < str1.length(); i++) {
+					
+					
+				//}
+   //for (int j = 0; j < str2.length(); j++) {
+	
+//}
+   
+   if(str1==str2) {
+	   
+	   System.out.println("Both are Equal");
+	   
+   }
+   else if(str1.equals(str2)) {
+	   System.out.println("Both strings are Different");
+   }
+   
+   else if(str1.equalsIgnoreCase(str2)) {
+	   
+	   System.out.println("Both Strings are Equal");
+   }
+   else {
+	   System.out.println("jfrvh");
+   }
+	   		
+   }
+   
+	}
+
+
