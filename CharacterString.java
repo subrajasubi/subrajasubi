d9d9f45e6c3ff293fafb1178a1d62a70603983ce
subrajasubi <95363799+subrajasubi@users.noreply.github.com>
2021-12-15 15:05:53 +0530

    Add files via upload

---
+++ CharacterString.java
@@ -0,0 +1,37 @@
+package week3.day1.assignments;
+
+public class CharacterString {
+	
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+        String text ="Java Exercise";
+		
+		for (int i = 0; i < text.length(); i++) {
+			
+			
+			
+			//System.out.println(text.charAt(i));
+			
+			
+			
+			if(text.charAt(i) == 'E') {
+				
+				System.out.println(text.charAt(5));
+			}
+			
+			if(text.charAt(i)== 's') {
+				
+				System.out.println(text.charAt(11));
+			}
+			
+		}
+
+
+	}
+
+}
