d3ec5f7979319ba603d5d0bc5022bc7d54dccc0d
subrajasubi <95363799+subrajasubi@users.noreply.github.com>
2021-12-15 15:02:17 +0530

    Add files via upload

---
+++ AxisBank.java
@@ -0,0 +1,21 @@
+package week3.day1.assignments;
+
+public class AxisBank {
+	
+	public void deposit() {
+		
+		System.out.println("6%");
+	}
+
+	public static void main(String[] args) {
+		
+		BankInfo bi = new BankInfo();
+		bi.saving();
+		bi.fixed();
+		bi.deposit();
+		
+		
+
+	}
+
+}
