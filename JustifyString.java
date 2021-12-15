86c4f8b27775582750f71e717e713835663c8378
subrajasubi <95363799+subrajasubi@users.noreply.github.com>
2021-12-15 15:03:36 +0530

    Add files via upload

---
+++ JustifyString.java
@@ -0,0 +1,38 @@
+package week3.day1.assignments;
+
+public class JustifyString {
+	
+	
+
+	
+	public static void main(String[] args) {
+		
+		String str3 = new String("Kutty");
+		 String str4 = new String("Kutty");
+		 
+		 
+		// for (int i = 0; i < str3.length(); i++) {
+			 
+			//System.out.println(str3.charAt(i));
+			
+		//}
+		 
+		 //for (int j = 0; j < str4.length(); j++) {
+			
+			 //System.out.println(str4.charAt(j));
+	//	}
+		 
+		 if(str3.equals(str4)) {
+			 
+			 
+			 System.out.println(" Same text");
+		 }
+		 else
+			 System.out.println("Diff text");
+		 
+		 System.out.println("***");
+		
+
+	}
+
+}
